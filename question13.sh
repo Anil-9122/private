9ba75e3af370d4462d52d76b6a98e776b92899b0
Anil-9122 <anilshrestha0012@gmail.com>
2025-03-23 17:25:17 +1030

    Update question13.sh

--- question13.sh
+++ question13.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 git checkout -b question13-branch
 rm question*.sh
 touch file13.txt
 git add file13.txt
-git commit -m "Added file13.txt"
+git commit -m "Added file13.txt and removed question*.sh files"
 git push origin question13-branch
