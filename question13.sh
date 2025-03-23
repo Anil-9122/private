c4c076e0492889a3e270fb16ee6a99954208112b
Anil-9122 <anilshrestha0012@gmail.com>
2025-03-23 17:18:04 +1030

    Update question13.sh

--- question13.sh
+++ question13.sh
@@ -1,8 +1,4 @@
-#!/bin/bash
-git checkout -b assignment-branch
-rm *.sh
-echo "This is file13.txt for Question 13." > file13.txt
-git add file13.txt
-git commit -m "Removed .sh files from other questions and added file13.txt"
-git push origin assignment-branch
-git branch -r
+git checkout branch2
+git stash pop
+git add file4
+git commit -m "Restored and commited uncommited changes to file4"
