c9a26ea5d819ac064f4f30f0a5d577d2d9f2ce71
Anil-9122 <anilshrestha0012@gmail.com>
2025-03-23 17:36:25 +1030

    Update question16.sh

--- question16.sh
+++ question16.sh
@@ -1,20 +1,4 @@
-#!/bin/bash
-
-# Fetch all remote branches
-git fetch --all
-
-# Checkout branch2
+git fetch origin
 git checkout branch2
-
-# Merge branch3 into branch2
-git merge branch3
-
-# Resolve any conflicts manually, then stage the resolved files
-# Example: git add file1.txt file2.txt
-git add <file1> <file2>  # Replace <file1> and <file2> with the actual filenames that had conflicts
-
-# Commit the merge resolution
-git commit -m "Merged branch3 into branch2 and resolved conflicts"
-
-# Delete branch3 locally
+git merge origin/branch3
 git branch -d branch3
